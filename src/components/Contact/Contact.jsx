a75a8258e67b5dba1baa71ffd1fd7fc7c12d6af2
Astbrqeg <eg.astbrq@gmail.com>
2023-06-11 18:01:43 +0300

    Portfolio

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -13,10 +13,10 @@ const Contact = () => {
 
         emailjs
             .sendForm(
-                "replace with service id",
-                "replace with template id",
+                "service_nok6duj",
+                "template_09x0usa",
                 form.current,
-                "replace with user id"
+                "REYvR7rbg7fLl9VpA"
             )
             .then(
                 (result) => {
@@ -30,16 +30,16 @@ const Contact = () => {
     };
     return (
         <div className='Contact' >
-            <div className='conreg'id='contact'>
+            <div className='conreg' id='contact'>
                 <div className='content'>
                     <h1>Contact Me</h1>
                     <form ref={form} onSubmit={sendEmail}>
                         <label>Name</label>
                         <input type="text" name="user_name" />
                         <label>Email</label>
-                        <input type="email" name="user_email" />
+                        <input type="email" name="user_email" required />
                         <label>Message</label>
-                        <textarea name="message" />
+                        <textarea name="message" required />
                         <input type="submit" value="Send" />
                     </form>
 
